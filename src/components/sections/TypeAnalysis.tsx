bc7fbe39cb8a05b501ee3f0c4d09e513de0180c5
Bexsta <tomasbee@gmx.de>
2025-08-26 12:45:54 +0200

    section type-analysis incl. repsonsive done, chnage global text elements to cursor default

--- src/components/sections/TypeAnalysis.tsx
+++ src/components/sections/TypeAnalysis.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./typeAnalysis.module.css";
 import Inner from "../utils/Inner";
 
@@ -10,30 +11,66 @@ export default function TypeAnalysis() {
 
           <div className={styles.content}>
             <div className={styles.per}>
-              <div className={styles.perText}>
-                <ul>
-                  <li>kreativ & gestaltungssicher</li>
-                  <li>experimentierfreudig & begeisterungsfähig</li>
-                  <li>eigenständig & lösungsorientiert</li>
-                  <li>visuell denkend</li>
-                  <li>empathisch & reflektiert</li>
-                  <li>Naturverbunden & sinnorientiert</li>
-                </ul>
+              <ul className={styles.perTextList}>
+                <li className={styles.perTextListItem}>
+                  kreativ & gestaltungssicher
+                </li>
+                <li className={styles.perTextListItem}>
+                  experimentierfreudig & begeisterungsfähig
+                </li>
+                <li className={styles.perTextListItem}>
+                  eigenständig & lösungsorientiert
+                </li>
+                <li className={styles.perTextListItem}>visuell denkend</li>
+                <li className={styles.perTextListItem}>
+                  empathisch & reflektiert
+                </li>
+                <li className={styles.perTextListItem}>
+                  Naturverbunden & sinnorientiert
+                </li>
+              </ul>
+              <div className={styles.perImageContainer}>
+                <Image
+                  src='/img/type/silhouette_colored_per.png'
+                  alt='Silhouette eines Mannes mit farbig hervorgehobenen Gehirnarealen'
+                  title='Silhoette eines Mannes für Typanalyse Vorteile'
+                  width={123}
+                  height={640}
+                  className={styles.perImage}
+                />
               </div>
-              <div className={styles.perImage}></div>
             </div>
             <div className={styles.contra}>
-              <div className={styles.contraImage}></div>
-              <div className={styles.contraText}>
-                <ul>
-                  <li>braucht klare Prioritäten bei komplexen Aufgaben</li>
-                  <li>frustriert bei sinnlosen oder monotonen Aufgabe</li>
-                  <li>reagiert sensibel auf Einschränkungen</li>
-                  <li>tut sich schwer mit rein Abstraktem</li>
-                  <li>emotional sensibel</li>
-                  <li>erschöpft in rein digitalen Kontexten</li>
-                </ul>
+              <div className={styles.contraImageContainer}>
+                <Image
+                  src='/img/type/silhouette_colored_contra.png'
+                  alt='Silhouette eines Mannes mit farbig hervorgehobenen Gehirnarealen'
+                  title='Silhoette eines Mannes für Typanalyse Nachteile'
+                  width={123}
+                  height={640}
+                  className={styles.contraImage}
+                />
               </div>
+              <ul className={styles.contraTextList}>
+                <li className={styles.contraTextListItem}>
+                  braucht klare Prioritäten bei komplexen Aufgaben
+                </li>
+                <li className={styles.contraTextListItem}>
+                  frustriert bei sinnlosen oder monotonen Aufgabe
+                </li>
+                <li className={styles.contraTextListItem}>
+                  reagiert sensibel auf Einschränkungen
+                </li>
+                <li className={styles.contraTextListItem}>
+                  tut sich schwer mit rein Abstraktem
+                </li>
+                <li className={styles.contraTextListItem}>
+                  emotional sensibel
+                </li>
+                <li className={styles.contraTextListItem}>
+                  erschöpft in rein digitalen Kontexten
+                </li>
+              </ul>
             </div>
           </div>
         </Inner>
