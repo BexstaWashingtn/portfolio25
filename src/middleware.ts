cb4fe14921bb0122bdaf7dc13824f1650ba1ffad
Bexsta <tomasbee@gmx.de>
2025-10-08 16:24:02 +0200

    extend whitelist with next Assets, Datas & Fonts, Pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,10 +8,29 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/locked", request.url));
   }
 
-  const url = request.nextUrl;
-  if (url.pathname.startsWith("/locked")) {
+  // Free Pages und Sources -------------------------------->
+  // ------------------------------------------------------->
+
+  const { pathname } = request.nextUrl;
+  // Framework-Assets & Optimizer freigeben
+  if (pathname.startsWith("/_next/")) {
     return NextResponse.next();
   }
+  // Statische Dateien/Fonts freigeben
+  if (
+    /\.(?:jpg|jpeg|png|gif|webp|svg|ico|css|js|map|json|txt|xml|woff2?|ttf|otf)$/.test(
+      pathname
+    )
+  ) {
+    return NextResponse.next();
+  }
+  // Deine offene Seite freigeben
+  if (pathname.startsWith("/locked")) {
+    return NextResponse.next();
+  }
+
+  // ------------------------------------------------------->
+  // ------------------------------------------------------->
 
   const secretBytes = Uint8Array.from(atob(secret), (c) => c.charCodeAt(0));
   const hmacKey = await crypto.subtle.importKey(
